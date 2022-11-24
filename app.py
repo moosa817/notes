ecf030886218cc7f8d7450e1cf70f1987df05b06
Moosa <Moosa>
2022-11-24 19:01:39 +0500

    modified

--- app.py
+++ app.py
@@ -346,4 +346,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0',debug=True, port=os.getenv("PORT", default=5000))
