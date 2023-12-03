78503a96e53d729005cfae19bc68b881af090f36
Moosa <computerknowledge817@gmail.com>
2023-12-04 00:58:02 +0500

    made email and username case insensitive (removed bug)

--- flask_files/signup.py
+++ flask_files/signup.py
@@ -7,19 +7,21 @@ import bcrypt
 # make a blueprints
 
 signup_page = blueprints.Blueprint(
-    'signup', __name__, static_folder='static', template_folder='templates')
+    "signup", __name__, static_folder="static", template_folder="templates"
+)
 
 
-@signup_page.route("/signup", methods=['POST', 'GET'])
+@signup_page.route("/signup", methods=["POST", "GET"])
 def signup():
     if session.get("username"):
-        return redirect(url_for('index.index'))
+        return redirect(url_for("index.index"))
 
     conn = mysql.connector.connect(
         host=config.host,
         user=config.user,
         passwd=config.passwd,
-        database=config.database)
+        database=config.database,
+    )
     cursor = conn.cursor()
     sql = "SELECT username,email FROM notes"
 
@@ -34,46 +36,95 @@ def signup():
         usernames.append(result[i][0])
         emails.append(result[i][1])
 
-    if request.method == 'POST':
-        email = request.form['email']
-        username = request.form['username']
-        pwd = request.form['pwd1']
-        confirm_pwd = request.form['pwd2']
+    if request.method == "POST":
+        email = request.form["email"].lower()
+        username = request.form["username"].lower()
+        pwd = request.form["pwd1"]
+        confirm_pwd = request.form["pwd2"]
 
-        emailRegex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        emailRegex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
         userRegex = "^[a-zA-Z0-9_.-]+$"
 
         if username == "" or email == "" or pwd == "" or confirm_pwd == "":
-            return render_template("signup.html", error="Please fill out all the forms", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Please fill out all the forms",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         elif len(username) > 150 or len(email) > 150 or len(pwd) > 150:
             return render_template("signup.html", error="Smth too big")
         elif len(pwd) < 8:
-            return render_template("signup.html", error="Password must be at least 8 characters long", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Password must be at least 8 characters long",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         elif pwd != confirm_pwd:
-            return render_template("signup.html", error="Passwords do not match", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Passwords do not match",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         # check for email
         elif not re.match(emailRegex, email):
-            return render_template("signup.html", error="Please enter a valid email address", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Please enter a valid email address",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         # check for username
         elif not re.match(userRegex, username):
-            return render_template("signup.html", error="Please enter a valid username", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Please enter a valid username",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         elif username in usernames:
-            return render_template("signup.html", error="Username already exists", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Username already exists",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         elif email in emails:
-            return render_template("signup.html", error="Email already exists", email=email, username=username, pwd=pwd, confirm_pwd=confirm_pwd)
+            return render_template(
+                "signup.html",
+                error="Email already exists",
+                email=email,
+                username=username,
+                pwd=pwd,
+                confirm_pwd=confirm_pwd,
+            )
         else:
-            pwd = pwd.encode('UTF-8')
+            pwd = pwd.encode("UTF-8")
             hash_pwd = bcrypt.hashpw(pwd, bcrypt.gensalt())
             # pwd = hash_pwd.decode('UTF-8')
             conn = mysql.connector.connect(
                 host=config.host,
                 user=config.user,
                 passwd=config.passwd,
-                database=config.database)
+                database=config.database,
+            )
             cursor = conn.cursor()
             sql = "INSERT INTO notes (username,email,password,pfp,email_confirmation) VALUES (%s,%s,%s,%s,%s)"
-            val = (username, email, hash_pwd,
-                   'static/img/Default.png', 'False')
+            val = (username, email, hash_pwd, "static/img/Default.png", "False")
             cursor.execute(sql, val)
             conn.commit()
             conn.close()
@@ -81,7 +132,7 @@ def signup():
             session["username"] = username
             session["email"] = email
             session["verified"] = False
-            session["pfp"] = '/static/img/Default.png'
+            session["pfp"] = "/static/img/Default.png"
             return redirect(url_for("index.index"))
 
     return render_template("signup.html")
